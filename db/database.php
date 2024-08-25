31aae8af3e368f91600d6a2ac35a908d4c118c1a
Nax <federico.diotallevi@studio.unibo.it>
2024-08-25 16:34:36 +0200

    [NEW]: implemented basic queries

--- db/database.php
+++ db/database.php
@@ -11,10 +11,11 @@ class DatabaseHelper
         }
     }
 
-    public function login(string $email, string $password) {
+    public function login(string $email, string $password)
+    {
         $user = $this->getLoginInfo($email);
 
-        if ($user && $user['password'] == $password){
+        if ($user && $user['password'] == $password) {
             return true;
         }
 
@@ -32,7 +33,7 @@ class DatabaseHelper
         $stmt->execute();
         $result = $stmt->get_result();
 
-        if($result->num_rows > 0)
+        if ($result->num_rows > 0)
             return $result->fetch_all(MYSQLI_ASSOC)[0]; // Return the first element of the array
 
         return false;
@@ -50,5 +51,36 @@ class DatabaseHelper
         $stmt->execute();
         $result = $stmt->get_result();
         return $result->fetch_all(MYSQLI_ASSOC)[0]; // Return the first element of the array
+    }
+
+    public function getAllMenuProducts()
+    {
+        $query = "SELECT * FROM MENU_PRODUCTS ORDER BY name";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getRecipe(int $menuProdId)
+    {
+        $query = "SELECT name as ingredientName, portionSize
+                    FROM ingredients left join stocked_up_products as stocked
+                        ON ingredientId = stocked.prodId
+                    WHERE ingredients.menuProdId = ? ;";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("i", $menuProdId);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getAllStockedProducts()
+    {
+        $query = "SELECT * FROM STOCKED_UP_PRODUCTS ORDER BY name";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
 }
